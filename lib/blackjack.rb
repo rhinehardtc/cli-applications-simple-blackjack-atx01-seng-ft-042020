31e5087e850ebe5d6341ab402fcba418e7a9221a
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-17 23:00:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,8 +25,8 @@ def end_game(number)
 end
 
 def initial_round
-  puts deal_card + deal_card
-  display_card_total(sum)
+  sum = deal_card + deal_card
+  puts display_card_total(sum)
 end
 
 def hit?
